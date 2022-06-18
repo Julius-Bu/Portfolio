11be86e61fa1d639b61734db4faeeb32f8956908
JuliusBot <75899898+Julius-Bu@users.noreply.github.com>
2022-06-18 11:01:24 +0300

    Create web.js

---
+++ js/web.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function navBar(){
+
+	if(document.all.navig.style.top == "0px"){
+		document.all.navig.style.position = "fixed";
+		document.all.navig.style.top = "0px";
+		document.all.navig.style.width = "100%";
+		document.all.navig.style.zIndex = "2";
+		
+	}
+}
