e6f673f8abc2ccf8c52d614c88d0462c0e83b15e
JuliusBot <75899898+Julius-Bu@users.noreply.github.com>
2022-06-29 22:03:11 +0300

    Update about.php

--- about.php
+++ about.php
@@ -7,88 +7,39 @@
 </head>
 
 <body>
-<h2>About the members</h2>
+<h2>About me</h2>
 
 <span>
 <center>
-<ul class="dropdown">
-  <span> Nakitto Lynnet</span>
-  <li class="dropdown-content">
-    <b>Hobbies : </b><i>Facebooking (if its a thing)</i><hr>
-	<b>Fav music genre : </b><i>Reggae</i><hr>
-	<b>Vacation location : </b><i>Canada</i><hr>
-	<b>Sci-fi or Romantic comedy : </b><i>Romantic comedy</i><hr>
-	<b>Auto-biography : </b><i>Am passionate and hate everyone</i><br>
-  </li>
-</ul>
+	<ul class="dropdown">
+	<span> Software Development</span>
+	<li class="dropdown-content">
+		<b>Hobbies : </b><i>Facebooking (if its a thing)</i><hr>
+		<b>Fav music genre : </b><i>Reggae</i><hr>
+		<b>Vacation location : </b><i>Canada</i><hr>
+		<b>Sci-fi or Romantic comedy : </b><i>Romantic comedy</i><hr>
+		<b>Auto-biography : </b><i>Am passionate and hate everyone</i><br>
+	</li>
+	</ul>
 
 
-<ul class="dropdown">
-  <span>Buwembo Julius</span>
-  <li class="dropdown-content">
-    <b>Hobbies : </b><i>Marathons</i><hr>
-	<b>Fav music genre : </b><i>Pop</i><hr>
-	<b>Vacation location : </b><i>South Africa</i><hr>
-	<b>Sci-fi or Romantic comedy : </b><i>Sci-fi</i><hr>
-	<b>Auto-biography : </b><i></i><br>
-  </li>
-</ul>
-
-<ul class="dropdown">
-  <span>Jude Okwerede</span>
-  <li class="dropdown-content">
-    <b>Hobbies : </b><i>Coding</i><hr>
-	<b>Fav music genre : </b><i>Trap</i><hr>
-	<b>Vacation location : </b><i>German</i><hr>
-	<b>Sci-fi or Romantic comedy : </b><i>Sci-fi</i><hr>
-	<b>Auto-biography : </b><i></i><br>
-	<!-- <aside><img src="../allimgs/hud.jpg" height="200px" width="200px"></aside> -->
-  </li>
-</ul>
-	</center>
-	</span>
-	
-	<span>
-	<center>
-<ul class="dropdown">
-  <span>Agaba Nicholas</span>
-  <li class="dropdown-content">
-    <b>Hobbies : </b><i>Gyming</i><hr>
-	<b>Fav music genre : </b><i>Afro pop</i><hr>
-	<b>Vacation location : </b><i>Sudan</i><hr>
-	<b>Sci-fi or Romantic comedy : </b><i>Romantic comedy</i><hr>
-	<b>Auto-biography : </b><i></i><br>
-  </li>
-</ul>
-
-
-<ul class="dropdown">
-  <span>Kiyemba Brian</span>
-  <li class="dropdown-content">
-    <b>Hobbies : </b><i></i><hr>
-	<b>Fav music genre : </b><i></i><hr>
-	<b>Vacation location : </b><i></i><hr>
-	<b>Sci-fi or Romantic comedy : </b><i></i><hr>
-	<b>Auto-biography : </b><i></i><br>
-  </li>
-</ul>
-
-<ul class="dropdown">
-  <span>Katusiime Maurice</span>
-  <li class="dropdown-content">
-    <b>Hobbies : </b><i></i><hr>
-	<b>Fav music genre : </b><i></i><hr>
-	<b>Vacation location : </b><i></i><hr>
-	<b>Sci-fi or Romantic comedy : </b><i></i><hr>
-	<b>Auto-biography : </b><i></i><br>
-  </li>
-</ul>
+	<ul class="dropdown">
+	<span>Computer Graphics</span>
+	<li class="dropdown-content">
+		<b>Hobbies : </b><i>Marathons</i><hr>
+		<b>Fav music genre : </b><i>Pop</i><hr>
+		<b>Vacation location : </b><i>South Africa</i><hr>
+		<b>Sci-fi or Romantic comedy : </b><i>Sci-fi</i><hr>
+		<b>Auto-biography : </b><i></i><br>
+	</li>
+	</ul>
 
 </center>
-</span>
+
+	</span>
 
 <div class="aboutcontent">
-	<b>Hobbies : </b><i></i><hr>
+	<b>H : </b><i></i><hr>
 	<b>Fav music genre : </b><i></i><hr>
 	<b>Vacation location : </b><i></i><hr>
 	<b>Sci-fi or Romantic comedy : </b><i></i><hr>
